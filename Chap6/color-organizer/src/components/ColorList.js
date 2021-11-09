cf2c7cb840d91c0d1003286d8ee07231a8c6cd52
Lars Krogsgård Hansen <lkh@dbc.dk>
2021-11-09 14:56:22 +0100

    Got some of it up and running rearding the colorlist

--- Chap6/color-organizer/src/components/ColorList.js
+++ Chap6/color-organizer/src/components/ColorList.js
@@ -5,16 +5,15 @@ import Color from "./Color";
  * colors are hook state data that are passed from the App.
  *
  * @param colors
- * @returns {JSX.Element}
+ * @param onRemoveColor
+ * @returns {JSX.Element|unknown[]}
  * @constructor
  */
-export default function ColorList({ colors = [] }) {
-  if(!colors.length) return <div>No Colors Listed.</div>;
+export default function ColorList({ colors = [], onRemoveColor = f => f }) {
+  if(!colors.length) return <div>No Colors Listed. (Add a Color)</div>;
   return (
-    <div>
-      {
-        colors.map(color => <Color key={color.id} {...color} />)
-      }
-    </div>
+    colors.map(color =>
+      <Color key={color.id} {...color} onRemove={onRemoveColor} />
+    )
   );
 }
