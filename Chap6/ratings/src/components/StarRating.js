3d27014a115cd8d1db01b54be5d120808af64626
Lars Krogsgård Hansen <lkh@dbc.dk>
2021-11-09 13:07:43 +0100

    Got the rating stars up and running

--- Chap6/ratings/src/components/StarRating.js
+++ Chap6/ratings/src/components/StarRating.js
@@ -1,18 +1,26 @@
-import React from "react";
-import { Star } from "Star";
+import {FaStar} from "react-icons/fa";
+import React, { useState } from "react";
 
 const createArray = length => [...Array(length)];
 
-export default function StarRating({ totalStars = 5 }) {
-  const [selectedStars] = useState(3);
+const Star = ({ selected = false, onSelect = f => f }) => (
+  <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
+);
+
+export default function StarRating({ style = {}, totalStars = 5, ...props}) {
+  const [selectedStars, setSelectedStars] = useState(3);
   return (
-    <>
+    <div style={{ padding: "5px", ...style }} {...props}>
       {createArray(totalStars).map((n, i) => (
-        <Star key={i} selected={selectedStars > i} />
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
       ))}
       <p>
         {selectedStars} of {totalStars} stars
       </p>
-    </>
+    </div>
   );
 }
